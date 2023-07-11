e7c0389d8c6c8a6d25a2fb76b5b09693f5c96ff9
Andrew Bell <fiixed@gmail.com>
2023-07-11 11:16:14 -0500

    refactor portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,119 +1,7 @@
 import React from 'react';
 import './portfolio.css';
-import IMG1 from '../../assets/nasa.png';
-import IMG2 from '../../assets/hikehunter.png';
-import IMG3 from '../../assets/pong.png';
-import IMG4 from '../../assets/projectmgmtgraphql.png';
-import IMG5 from '../../assets/ecommerceapi.png';
-import IMG6 from '../../assets/portfolio2023.png';
-import IMG7 from '../../assets/portfolio2021.png';
-import IMG8 from '../../assets/ecommerce.png';
-import IMG9 from '../../assets/hookstactoe.png';
-import IMG10 from '../../assets/reduxtactoe.png';
-import IMG11 from '../../assets/video_diary.png';
-import IMG12 from '../../assets/invest_austin.png';
-import IMG13 from '../../assets/agview.png';
+import data from '../../utils/constant.js';
 
-const data = [
-  {
-    id: 1,
-    image: IMG1,
-    title: 'NASA Mission Control API',
-    sub: 'Mostly backend project using Express, Node, Mongo w/ React frontend.  Hosted on AWS w/ Docker.',
-    github: 'https://github.com/fiixed/nasa-mission-control',
-    demo: 'https://andrewsherrod.com/',
-  },
-  {
-    id: 2,
-    image: IMG2,
-    title: 'Hike Hunter',
-    sub: 'Full stack project using EJS, Express and Mongo.',
-    github: 'https://github.com/fiixed/hikehunter',
-    demo: 'https://hike-hunter.onrender.com/',
-  },
-  {
-    id: 3,
-    image: IMG3,
-    title: 'Multi-player Pong',
-    sub: 'Multi-player Pong game using Sockets',
-    github: 'https://github.com/fiixed/multi-player-pong-sockets',
-  },
-  {
-    id: 4,
-    image: IMG4,
-    title: 'Project Mgmt GraphQL API',
-    sub: 'Mostly backend project using GraphQL w/ React frontend.',
-    github: 'https://github.com/fiixed/project-mgmt-app-graphql/tree/main',
-    demo: 'https://project-mgmt-app-graphql-frontend.onrender.com/',
-  },
-  {
-    id: 5,
-    image: IMG5,
-    title: 'NodeJS E-Commerce API',
-    sub: 'Backend-only API for e-commerce site using Express, Cloudinary, Stripe and Mongo (Frontend coming soon).',
-    github: 'https://github.com/fiixed/nodejs-ecommerce-api',
-  },
-  {
-    id: 6,
-    image: IMG6,
-    title: 'Andrew Sherrod 2023 Portfolio',
-    sub: 'Frontend-only React portfolio refreshed for 2023.  Built from scratch, no templates.',
-    github: 'https://github.com/fiixed/react-portfolio-2023',
-  },
-  {
-    id: 7,
-    image: IMG7,
-    title: 'Andrew Bell 2021 Portfolio',
-    sub: 'Frontend-only React portfolio built from scratch in 2021.  Hosted on Firebase.  (Bell is my maiden name).',
-    github: 'https://github.com/fiixed/react-portfolio',
-    demo: 'https://andrew-bell-react-portfolio.web.app/',
-  },
-  {
-    id: 8,
-    image: IMG8,
-    title: 'Full Stack E-commerce w/ Stripe',
-    sub: 'Capstone project built with React, Redux, Ant, Node, Express, Cloudinary, Stripe, Firebase and Mongo',
-    githubfront: 'https://github.com/fiixed/react-whitelabel-ecommerce-front',
-    githubback: 'https://github.com/fiixed/react-whitelabel-ecommerce-back',
-  },
-  {
-    id: 9,
-    image: IMG9,
-    title: 'React Hooks Tac Toe',
-    sub: 'I built two versions of the same game, one using Hooks and another using Redux for state management',
-    github: 'https://github.com/fiixed/react-hooks-tac-toe',
-    demo: 'https://react-hooks-tac-toe.web.app/',
-  },
-  {
-    id: 10,
-    image: IMG10,
-    title: 'React Redux Tac Toe',
-    sub: 'I built two versions of the same game, one using Hooks and another using Redux for state management',
-    github: 'https://github.com/fiixed/react-redux-tac-toe',
-    demo: 'https://react-redux-tac-toe-9.web.app/',
-  },
-  {
-    id: 11,
-    image: IMG11,
-    title: 'Video Diary',
-    sub: 'Video diary mobile app built using Flutter',
-    github: 'https://github.com/fiixed/Video-Diary-Flutter',
-  },
-  {
-    id: 12,
-    image: IMG12,
-    title: 'Invest Austin',
-    sub: 'Google Tango AR app built for the City of Austin.  Was presented at Unite 17, see link below for conference Apresentation',
-    youtube: 'https://www.youtube.com/watch?v=QDQsg1hPa38',
-  },
-  {
-    id: 13,
-    image: IMG13,
-    title: 'AgView',
-    sub: 'Google Tango AR app built for the City of Austin.  Was presented at Unite 17, see link below for conference Apresentation',
-    appstore: 'https://www.youtube.com/watch?v=QDQsg1hPa38',
-  },
-];
 
 const Portfolio = () => {
   return (
@@ -134,6 +22,7 @@ const Portfolio = () => {
             demo,
             youtube,
             appstore,
+            homepage,
           }) => {
             return (
               <article key={id} className="portfolio__item">
@@ -168,13 +57,17 @@ const Portfolio = () => {
                       App Store
                     </a>
                   ) : null}
+                  {homepage ? (
+                    <a href={homepage} className="btn" target="_blank">
+                      Home Page
+                    </a>
+                  ) : null}
 
                   {demo && id != 8 ? (
                     <a href={demo} className="btn btn-primary" target="_blank">
                       Live Demo
                     </a>
                   ) : null}
-                  
                 </div>
               </article>
             );
---
+++ src/utils/constant.js
@@ -0,0 +1,151 @@
+import IMG1 from '../assets/nasa.png';
+import IMG2 from '../assets/hikehunter.png';
+import IMG3 from '../assets/pong.png';
+import IMG4 from '../assets/projectmgmtgraphql.png';
+import IMG5 from '../assets/ecommerceapi.png';
+import IMG6 from '../assets/portfolio2023.png';
+import IMG7 from '../assets/portfolio2021.png';
+import IMG8 from '../assets/ecommerce.png';
+import IMG9 from '../assets/hookstactoe.png';
+import IMG10 from '../assets/reduxtactoe.png';
+import IMG11 from '../assets/video_diary.png';
+import IMG12 from '../assets/invest_austin.png';
+import IMG13 from '../assets/agview.png';
+import IMG14 from '../assets/sirius_kickoff.png';
+import IMG15 from '../assets/davis_vision.png';
+import IMG16 from '../assets/tram_tracker.png';
+import IMG17 from '../assets/flutterportfolio.png';
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'NASA Mission Control API',
+    sub: 'Mostly backend project using Express, Node, Mongo w/ React frontend.  Hosted on AWS w/ Docker.',
+    github: 'https://github.com/fiixed/nasa-mission-control',
+    demo: 'https://andrewsherrod.com/',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Hike Hunter',
+    sub: 'Full stack project using EJS, Express and Mongo.',
+    github: 'https://github.com/fiixed/hikehunter',
+    demo: 'https://hike-hunter.onrender.com/',
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Multi-player Pong',
+    sub: 'Multi-player Pong game using Sockets',
+    github: 'https://github.com/fiixed/multi-player-pong-sockets',
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Project Mgmt GraphQL API',
+    sub: 'Mostly backend project using GraphQL w/ React frontend.',
+    github: 'https://github.com/fiixed/project-mgmt-app-graphql/tree/main',
+    demo: 'https://project-mgmt-app-graphql-frontend.onrender.com/',
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'NodeJS E-Commerce API',
+    sub: 'Backend-only API for e-commerce site using Express, Cloudinary, Stripe and Mongo (Frontend coming soon).',
+    github: 'https://github.com/fiixed/nodejs-ecommerce-api',
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Andrew Sherrod 2023 Portfolio',
+    sub: 'Frontend-only React portfolio refreshed for 2023.  Built from scratch, no templates.',
+    github: 'https://github.com/fiixed/react-portfolio-2023',
+  },
+  {
+    id: 7,
+    image: IMG7,
+    title: 'Andrew Bell 2021 Portfolio',
+    sub: 'Frontend-only React portfolio built from scratch in 2021.  Hosted on Firebase.  (Bell is my maiden name).',
+    github: 'https://github.com/fiixed/react-portfolio',
+    demo: 'https://andrew-bell-react-portfolio.web.app/',
+  },
+  {
+    id: 8,
+    image: IMG8,
+    title: 'Full Stack E-commerce w/ Stripe',
+    sub: 'Capstone project built with React, Redux, Ant, Node, Express, Cloudinary, Stripe, Firebase and Mongo',
+    githubfront: 'https://github.com/fiixed/react-whitelabel-ecommerce-front',
+    githubback: 'https://github.com/fiixed/react-whitelabel-ecommerce-back',
+  },
+  {
+    id: 9,
+    image: IMG9,
+    title: 'React Hooks Tac Toe',
+    sub: 'I built two versions of the same game, one using Hooks and another using Redux for state management',
+    github: 'https://github.com/fiixed/react-hooks-tac-toe',
+    demo: 'https://react-hooks-tac-toe.web.app/',
+  },
+  {
+    id: 10,
+    image: IMG10,
+    title: 'React Redux Tac Toe',
+    sub: 'I built two versions of the same game, one using Hooks and another using Redux for state management',
+    github: 'https://github.com/fiixed/react-redux-tac-toe',
+    demo: 'https://react-redux-tac-toe-9.web.app/',
+  },
+  {
+    id: 11,
+    image: IMG11,
+    title: 'Video Diary',
+    sub: 'Video diary mobile app built using Flutter',
+    github: 'https://github.com/fiixed/Video-Diary-Flutter',
+  },
+  {
+    id: 12,
+    image: IMG12,
+    title: 'Invest Austin',
+    sub: 'Google Tango AR app built for the City of Austin.  Was presented at Unite 17, see link below for conference Apresentation',
+    youtube: 'https://www.youtube.com/watch?v=QDQsg1hPa38',
+  },
+  {
+    id: 13,
+    image: IMG13,
+    title: 'AgView',
+    sub: 'Agricultural animal tracking app for farmers built for the National Pork Board.  Built using Ionic',
+    appstore: 'https://apps.apple.com/tn/app/agview/id1401972444',
+  },
+  {
+    id: 14,
+    image: IMG14,
+    title: 'Sirius Kickoff',
+    sub: 'The Sirius Kickoff conference mobile companionion app was built for the Sirius Kickoff conference .  Built using React Native',
+    appstore:
+      'https://play.google.com/store/apps/details?id=com.sirius.kickoff2020&hl=en_US&gl=US',
+  },
+  {
+    id: 15,
+    image: IMG15,
+    title: 'Davis Vision',
+    sub: 'Mobile app built for Davis Vision including geo and nav.  Built using Ionic',
+    homepage: 'https://davisvision.com/app/',
+  },
+  {
+    id: 16,
+    image: IMG16,
+    title: 'tramTracker',
+    sub: 'Worked on the tramTracker Android team during my time at WME/Appscore',
+    appstore:
+      'https://play.google.com/store/apps/details?id=com.yarratrams.tramtracker&hl=en_US',
+  },
+  {
+    id: 17,
+    image: IMG17,
+    title: 'Flutter for Web Portfolio',
+    sub: 'Older portfolio built using Flutter for Web when it was still in beta.  Hosted on Github Pages',
+    github: 'https://github.com/fiixed/flutter_web_portfolio',
+    demo: 'https://fiixed.github.io/#/',
+  },
+];
+
+export default data;
